1955d88a04ab01313fbdd21f271668664e79f507
BloodhoundNLD <126972936+BloodhoundNL@users.noreply.github.com>
2023-10-23 19:32:02 +0200

    tweak(Update-sunday-events.task): Daylight savings

--- src/tasks/sunday-events.task.ts
+++ src/tasks/sunday-events.task.ts
@@ -53,7 +53,7 @@ export class SundayEventScheduleJob implements ScheduleJob {
     const sundayStartDate = dayjs()
       .add(1, "week")
       .day(0)
-      .hour(17)
+      .hour(18)
       .minute(0)
       .second(0);
     const sundayEndDate = sundayStartDate.add(4, "hour");
